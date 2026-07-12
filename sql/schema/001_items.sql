0260b9f41806b354dbb9c2e658e8c72fa2b7453d
Carl Menditta <menditta1@gmail.com>
2026-07-12 15:10:02 +0100

    fix sql syntax

--- sql/schema/001_items.sql
+++ sql/schema/001_items.sql
@@ -3,8 +3,8 @@
 CREATE TABLE items(
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     name TEXT NOT NULL UNIQUE,
-    created_at INTEGER NOT NULL DEFAULT unixepoch(),
-    updated_at INTEGER NOT NULL DEFAULT unixepoch()
+    created_at INTEGER NOT NULL DEFAULT (unixepoch()),
+    updated_at INTEGER NOT NULL DEFAULT (unixepoch())
 ) STRICT;
 
 -- +goose Down
